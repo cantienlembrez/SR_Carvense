891bdf18b670a378c3254d38ab8f7e8dc24eed73
Cantien Lembrez <cantien.lembrez@utoulouse.fr>
2026-03-18 20:43:45 +0100

    Trioecy model added

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ from BasicFunctions import *
 from Models import *
 
 def CorrThetaF(n, ThetaF):
-    # biais correction see (Xu and Fu 2004)
+    # biais correction see Xu and Fu 2004
     if ThetaF>15:
         A = (1.1675 + 3.3232/n + 63.698/(n**2))
         B = 0.2569
@@ -16,13 +16,18 @@ def CorrThetaF(n, ThetaF):
     UnbiasedThetaF = ((np.sqrt(B**2 + 4*A*ThetaF) - B)/(2*A))**2
     return UnbiasedThetaF
 
-def run(N, IDmsats, Musat, Muiloc, Gmax, Sm=0.5):
+def run(N, IDmsats, Musat, Muiloc, Gmax, Sm=0.5, em = 0.65, s = 0.44, d = 0.1, a = 5, g = 1.1):
     """N: number of individuals,
     IDmsats: scaled position of microsatellites loci on chromosome (increasing order)
     Musat: microsatellites mutation rate
     Muiloc: mutation rate for infinite allele loci
     Gmax: generation ending simulation
     Sm: survival rate of male in model 2
+    em: Proportion of male aborted pollen due to CMS
+    s: hermaphrodite selfing rate
+    d: prop of selfing seed aborted due to inbreeding depretion
+    a: ratio  1  male pollen /  1 hermaphrodite pollen
+    g: ratio 1 female seeds /  1 hermaphrodite seeds
     """
     N2 = N*2
     nMsats = len(IDmsats)
@@ -32,17 +37,53 @@ def run(N, IDmsats, Musat, Muiloc, Gmax, Sm=0.5):
     #the first column contain the information for the sex determination locus 0 : female recessive
     # and the second contain an neutral locus (infinite allele locus)
     nuc = np.zeros((N2, nMsats+2),dtype=int)
-    #infinite allele model one neutral locus
-    cyt = np.zeros((N),dtype=int)
+    #infinite allele model one neutral locus first locus for CMS 1 or wild cytotype 0
+    cyt = np.zeros((N, 2),dtype=int)
+    # cyt = np.zeros((N),dtype=int)
 
     tmp_nuc = np.zeros((N2, nMsats+2),dtype=int)
-    tmp_cyt = np.zeros((N),dtype=int)
+    tmp_cyt = np.zeros((N, 2),dtype=int)
     repeats = np.random.poisson(20, nMsats) + 5
-    for chr_id in range(N2):
-        nuc[chr_id,2:] = repeats
-        #initial sex ratio of 0.5
-        if chr_id<N and chr_id%2==1:
-            nuc[chr_id,0] = 1
+
+
+    Pollen = [[-1],[0]] # first line containt id; second cumulative weight to pollen/Ovule contribution of individuals
+    Ovules = [[-1],[0]]
+    #initializing sex ratio at 1/4 * 4
+    O_ID = 0
+    P_ID = 0
+    HO = 1/g
+    HP = 1/a
+    MCMSP = 1-em
+    for i in range(N):
+        nuc[i*2,2:] = repeats
+        nuc[i*2+1,2:] = repeats
+        if i<N//4:
+            # hermaphrodite plants
+            Ovules[0].append(i)
+            Ovules[1].append(Ovules[1][O_ID] + HO)
+            Pollen[0].append(i)
+            Pollen[1].append(Pollen[1][P_ID] + HP)
+            O_ID+=1
+            P_ID+=1
+        elif i>=N//4 and i<N//2:
+            nuc[i*2+1,0] = 1
+            # male plants
+            Pollen[0].append(i)
+            Pollen[1].append(Pollen[1][P_ID] + 1)
+            P_ID+=1
+        elif i>=N//2 and i<(N//4)*3:
+            # female plants
+            cyt[i, 0] = 1
+            Ovules[0].append(i)
+            Ovules[1].append(Ovules[1][O_ID] + 1)
+            O_ID+=1
+        else:
+            # male + CMS plants
+            nuc[i*2+1,0] = 1
+            cyt[i, 0] = 1
+            Pollen[0].append(i)
+            Pollen[1].append(Pollen[1][P_ID] + MCMSP)
+            P_ID+=1
 
     f = open("Simulations/"+datetime.today().strftime('%Y-%m-%d_%H:%M') +".txt", "a")
     #writing parameters and initial state
@@ -53,10 +94,13 @@ def run(N, IDmsats, Musat, Muiloc, Gmax, Sm=0.5):
     f.write("      " + "   ".join(["_".join([str(i), str(IDmsats[i])],)  for i in range(nMsats)])+"\n")
     f.write("Anc   "+"   ".join([str(r) for r in repeats])+"\n")
 
+    #s_d = s*(1-d)
     for g in range(1, Gmax+1):
         #gen_incr(N, N2, nMsats, Musat, Muiloc, nuc, cyt, tmp_nuc, tmp_cyt)
-        gen_incr_m2(N, N2, nMsats, Musat, Muiloc, nuc, cyt, tmp_nuc, tmp_cyt, Sm)
+        #gen_incr_m2(N, N2, nMsats, Musat, Muiloc, nuc, cyt, tmp_nuc, tmp_cyt, Sm)
+        Ovules, Pollen = gen_incr_CMS(N, N2, nMsats, Musat, Muiloc, HO, HP, MCMSP, s, d, nuc, cyt, Ovules, Pollen, tmp_nuc, tmp_cyt)
         nuc, cyt = np.copy(tmp_nuc), np.copy(tmp_cyt)
+
 
     Nm = np.sum(nuc[1:N2:2, 0])
     Nf = N-Nm
@@ -67,8 +111,7 @@ def run(N, IDmsats, Musat, Muiloc, Gmax, Sm=0.5):
     freqs = np.unique(nuc[:, 1], return_counts=True)[1]/N2
     He = 1 - sum(freqs**2)
     theta = He/((1-He))
-    print(He)
-    print("theta from infinite allele locus :" +str(theta))
+    print("theta from infinite allele locus :" + str(theta))
     print("estimated effective pop size : " + str(theta/(4*Muiloc)))
     for i in range(nMsats):
         f.write(IDmsats[i]+":\n")
@@ -95,7 +138,7 @@ def run(N, IDmsats, Musat, Muiloc, Gmax, Sm=0.5):
     print("population size estimated from sex ratio : " + str(Ne))
 
     #effective size estimation for cytoplasmic loci
-    alleles, freqs  = np.unique(cyt, return_counts=True)
+    alleles, freqs  = np.unique(cyt[:,1], return_counts=True)
     freqs=freqs/N
     He = 1 - sum(freqs**2)
     thetaC = He/((1-He))
@@ -104,6 +147,23 @@ def run(N, IDmsats, Musat, Muiloc, Gmax, Sm=0.5):
     print("number of female:",str(Nf))
     print("estimated pop size:",str(Nf/2))
     f.close()
+    Nm = 0
+    Nf = 0
+    Nh = 0
+    Nmcms =0
+    for i in range(N):
+        if nuc[i*2+1, 0]==0 and cyt[i, 0]==0:
+            Nh+=1
+        elif nuc[i*2+1, 0]==1 and cyt[i, 0]==0:
+            Nm+=1
+        elif nuc[i*2+1, 0]==0 and cyt[i, 0]==1:
+            Nf+=1
+        else:
+            Nmcms+=1
+    print("count Nm", Nm)
+    print("count Nf", Nf)
+    print("count Nh", Nh)
+    print("count Nmcms", Nmcms)
     return
 
 try:
@@ -111,7 +171,7 @@ try:
 except:
     pass
 
-Musat = 1e-2
-N = 2000
-print("theta theo : " + str(4*N*Musat))
-run(N,[chr(i+65) for i in range(25)], Musat, Musat, 9000)
+Mu = 1e-3
+N = 1000
+print("theta theo (if Ne = N): " + str(4*N*Mu))
+run(N,[chr(i+65) for i in range(1)], Mu, Mu, 4000)
--- Models.py
+++ Models.py
@@ -25,7 +25,7 @@ def gen_incr(N, N2, nMsats, Musat, Muiloc, nuc, cyt, tmp_nuc, tmp_cyt):
     mutation_msat(N2, nMsats, Musat, tmp_nuc[:,2:])
     mutations_iloc(N2, Muiloc, tmp_nuc[:,1])
     #mutate all the cytotypes
-    tmp_cyt = mutations_iloc(N, Muiloc, tmp_cyt)
+    mutations_iloc(N, Muiloc, tmp_cyt[:, 1])
     return
 
 
@@ -54,5 +54,83 @@ def gen_incr_m2(N, N2, nMsats, Musat, Muiloc, nuc, cyt, tmp_nuc, tmp_cyt, Sm):
     mutation_msat(N2, nMsats, Musat, tmp_nuc[:,2:])
     mutations_iloc(N2, Muiloc, tmp_nuc[:,1])
     #mutate all the cytotypes
-    tmp_cyt = mutations_iloc(N, Muiloc, tmp_cyt)
+    mutations_iloc(N, Muiloc, tmp_cyt[:, 1])
     return
+
+
+def choiceFromTab(tab, tablen, cumul):
+    rnum = np.random.uniform(low = 0, high = cumul)
+    i=0
+    while tab[1][i]<rnum:
+        i+=1;
+        #optimizable using minimal value and difference;
+    return tab[0][i]
+
+
+def gen_incr_CMS(N, N2, nMsats, Musat, Muiloc, HO, HP, MCMSP, s, d, nuc, cyt, Ovules, Pollen, tmp_nuc, tmp_cyt):
+    """make one life cycle stop after offspring birth"""
+
+    tmp_Ovules = [[-1],[0]]
+    tmp_Pollen = [[-1],[0]]
+    O_ID = 0
+    P_ID = 0
+
+    OvLen = len(Ovules[0])-1
+    PoLen = len(Pollen[0])-1
+    OvCum = Ovules[1][OvLen]
+    PoCum = Pollen[1][PoLen]
+
+    i=0
+    while i<N:
+        aborted = False
+        #Ovule choice
+        p1_id = choiceFromTab(Ovules, OvLen, OvCum)
+        # hermphrodite has a chance of selfing
+        if cyt[p1_id,0]==0 and np.random.rand()<=s:
+            if np.random.rand()<=d:
+                aborted=True
+            else:
+                p2_id = p1_id
+        else:
+            p2_id = choiceFromTab(Pollen, PoLen, PoCum)
+
+        if not(aborted):
+            #recombination
+            tmp_nuc[i*2,:] = recombi_ind(nuc[(p1_id*2,p1_id*2+1),], nMsats)
+            tmp_nuc[i*2+1,:] = recombi_ind(nuc[(p2_id*2,p2_id*2+1),], nMsats)
+
+            #cytotype transmission
+            tmp_cyt[i,:] = cyt[p1_id, :]
+
+            # Attributing weight
+            if tmp_nuc[i*2+1,0]==0 and cyt[p1_id,0]==0:
+                #hermaphrodite
+                tmp_Ovules[0].append(i)
+                tmp_Ovules[1].append(tmp_Ovules[1][O_ID] + HO)
+                tmp_Pollen[0].append(i)
+                tmp_Pollen[1].append(tmp_Pollen[1][P_ID] + HP)
+                O_ID+=1
+                P_ID+=1
+            elif  tmp_nuc[i*2+1,0]==1 and cyt[p1_id,0]==0:
+                #male
+                tmp_Pollen[0].append(i)
+                tmp_Pollen[1].append(tmp_Pollen[1][P_ID] + 1)
+                P_ID+=1
+            elif tmp_nuc[i*2+1,0]==0 and cyt[p1_id,0]==1:
+                #female
+                tmp_Ovules[0].append(i)
+                tmp_Ovules[1].append(tmp_Ovules[1][O_ID] + 1)
+                O_ID+=1
+            else:
+                #male + CMS
+                tmp_Pollen[0].append(i)
+                tmp_Pollen[1].append(tmp_Pollen[1][P_ID] + MCMSP)
+                P_ID+=1
+            i+=1
+
+    #mutation of all offspring
+    mutation_msat(N2, nMsats, Musat, tmp_nuc[:,2:])
+    mutations_iloc(N2, Muiloc, tmp_nuc[:,1])
+    #mutate all the cytotypes
+    mutations_iloc(N, Muiloc, tmp_cyt[:, 1])
+    return tmp_Ovules, tmp_Pollen
